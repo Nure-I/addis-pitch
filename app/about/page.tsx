8184f5bf91bc6eca6cdf70cafe79b3a9832f200f
Nure-I <ibrahimsaladin1@gmail.com>
2024-12-10 22:35:51 +0300

    addis pitch first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div className="text-3xl">About</div>;
+};
+
+export default page;
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://dadbef30c3a47d0532765d4e0654f970@o4504201813426176.ingest.us.sentry.io/4508445149167616",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
